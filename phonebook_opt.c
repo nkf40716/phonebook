a2cdf0718f9ed73b9e067ec8378fe17e359f8b0e
unknown <janaltsai@cyberon.com.tw>
2017-04-13 11:33:28 +0800

    log

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,4 +1,7 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "phonebook_opt.h"
 
@@ -12,4 +15,17 @@ entry *findName(char lastName[], entry *pHead)
 entry *append(char lastName[], entry *e)
 {
     return NULL;
+}
+
+// Optimization By hash
+unsigned int BKDRHash(char *str)
+{
+    unsigned int seed = 131; // 31 131 1313 13131 131313 etc..
+    unsigned int hash = 0;
+
+    while (*str) {
+        hash = hash * seed + (*str++);
+    }
+
+    return (hash & 0x7FFFFFFF);
 }
